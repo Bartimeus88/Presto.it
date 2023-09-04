355df870e69b235e0ffea6a112928904b2bd7501
Gellart <gellart0@gmail.com>
2023-09-04 22:31:07 +0200

    creazione progetto - installazione di bootstrap - installazione di Fortify - file storage

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <h1>Hello, world!</h1>
+
+    {{ $slot }}
+
+</body>
+
+</html>
