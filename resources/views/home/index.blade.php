6f0c6e2b6c8b1c6d907c16d1feb949201c5c9edc
Bartimeus88 <luca.lovisa8@gmail.com>
2023-09-05 21:20:51 +0200

    Update index.blade.php
    bottone annunci

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,5 +1,19 @@
 <x-layout>
-   <x-navbar></x-navbar>
+   <x-navbar/>
 
-   
+    <h1 class="display-1 text-center my-5">Presto.it</h1>
+
+    @if (auth()->check()) 
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center display-4 mb-4">
+                Crea il tuo annuncio
+            </div>
+            <a class="btn btn-lg btn-primary" href="/login">Crea il tuo annuncio personalizzato</a>
+        </div>
+    </div>
+   @endif
+
+
+
 </x-layout>
