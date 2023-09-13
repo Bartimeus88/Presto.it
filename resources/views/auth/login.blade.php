fed6f8feb0fec996ad1b70bd24f2b25ea788dac6
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-09-14 00:50:57 +0200

    fronted - guardare file txt modifiche

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-<div class="container my-5">
+    {{-- <div class="container my-5">
     <div class="row">
         <div class="col-12">
             <div class="card p-5">
@@ -26,7 +26,215 @@
             </div>
          </div>
      </div>
-</div>
+    </div> --}}
+
+    {{-- <section class="section-login">
+        <div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+            <div class="card card0 border-0">
+                <div class="row d-flex">
+                    <div class="col-lg-6">
+                        <div class="card1 pb-5">
+                            <div class="row px-3 justify-content-center mt-4 mb-5 border-line">
+                                <img src="https://i.imgur.com/uNGdWHi.png" class="image-login">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card2 card border-0 px-4 py-5">
+                            <div class="row mb-4 px-3">
+                                <h6 class="mb-0 mr-4 mt-2">Sign in with</h6>
+                                <div class="facebook text-center mr-3">
+                                    <div class="fa fa-facebook"></div>
+                                </div>
+                                <div class="twitter text-center mr-3">
+                                    <div class="fa fa-twitter"></div>
+                                </div>
+                                <div class="linkedin text-center mr-3">
+                                    <div class="fa fa-linkedin"></div>
+                                </div>
+                            </div>
+                            <div class="row px-3 mb-4">
+                                <div class="line"></div>
+                                <small class="or text-center">Or</small>
+                                <div class="line"></div>
+                            </div>
+                            <div class="row px-3">
+                                <label class="mb-1">
+                                    <h6 class="mb-0 text-sm">Email Address</h6>
+                                </label>
+                                <input class="mb-4" type="text" name="email"
+                                    placeholder="Enter a valid email address">
+                            </div>
+                            <div class="row px-3">
+                                <label class="mb-1">
+                                    <h6 class="mb-0 text-sm">Password</h6>
+                                </label>
+                                <input type="password" name="password" placeholder="Enter password">
+                            </div>
+                            <div class="row px-3 mb-4">
+                                <div class="custom-control custom-checkbox custom-control-inline">
+                                    <input id="chk1" type="checkbox" name="chk" class="custom-control-input">
+                                    <label for="chk1" class="custom-control-label text-sm">Remember me</label>
+                                </div>
+                                <a href="#" class="ml-auto mb-0 text-sm">Forgot Password?</a>
+                            </div>
+                            <div class="row mb-3 px-3">
+                                <button type="submit" class="btn btn-blue text-center">Login</button>
+                            </div>
+                            <div class="row mb-4 px-3">
+                                <small class="font-weight-bold">Don't have an account? <a
+                                        class="text-danger ">Register</a></small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="bg-blue py-4">
+                    <div class="row px-3">
+                        <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2019. All rights reserved.</small>
+                        <div class="social-contact ml-4 ml-sm-auto">
+                            <span class="fa fa-facebook mr-4 text-sm"></span>
+                            <span class="fa fa-google-plus mr-4 text-sm"></span>
+                            <span class="fa fa-linkedin mr-4 text-sm"></span>
+                            <span class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section> --}}
+
+    {{-- <div class="container-fluid">
+        <div class="bg-img">
+            <div class="content">
+                <header>Login Form</header>
+                <form action="#">
+                    <div class="field">
+                        <span class="fa fa-user"></span>
+                        <input type="text" required placeholder="Email or Phone">
+                    </div>
+                    <div class="field space">
+                        <span class="fa fa-lock"></span>
+                        <input type="password" class="pass-key" required placeholder="Password">
+                        <span class="show">SHOW</span>
+                    </div>
+                    <div class="pass">
+                        <a href="#">Forgot Password?</a>
+                    </div>
+                    <div class="field">
+                        <input type="submit" value="LOGIN">
+                    </div>
+                </form>
+                <div class="login">Or login with</div>
+                <div class="links">
+                    <div class="facebook">
+                        <i class="fab fa-facebook-f"><span>Facebook</span></i>
+                    </div>
+                    <div class="instagram">
+                        <i class="fab fa-instagram"><span>Instagram</span></i>
+                    </div>
+                </div>
+                <div class="signup">Don't have account?
+                    <a href="#">Signup Now</a>
+                </div>
+            </div>
+        </div>
+    </div> --}}
+
+    {{-- possibili nuovi footer --}}
+    {{-- <div class="bg-img">
+        <div class="content">
+            <header>Login Form</header>
+            <form action="#">
+                <div class="field">
+                    <span class="fa fa-user"></span>
+                    <input type="text" required placeholder="Email or Phone">
+                </div>
+                <div class="field space">
+                    <span class="fa fa-lock"></span>
+                    <input type="password" class="pass-key" required placeholder="Password">
+                    <span class="show">SHOW</span>
+                </div>
+                <div class="pass">
+                    <a href="#">Forgot Password?</a>
+                </div>
+                <div class="field">
+                    <input type="submit" value="LOGIN">
+                </div>
+            </form>
+            <div class="login">Or login with</div>
+            <div class="links">
+                <div class="facebook">
+                    <i class="fab fa-facebook-f"><span>Facebook</span></i>
+                </div>
+                <div class="instagram">
+                    <i class="fab fa-instagram"><span>Instagram</span></i>
+                </div>
+            </div>
+            <div class="signup">Don't have account?
+                <a href="#">Signup Now</a>
+            </div>
+        </div>
+    </div> --}}
+
+    <div class="container-fluid mb-5 bg-grey">
+        <section class="vh-100">
+            <div class="container py-5 h-100">
+                <div class="row d-flex align-items-center justify-content-center h-100">
+                    <div class="col-md-8 col-lg-7 col-xl-6">
+                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+                            class="img-fluid" alt="Phone image">
+                    </div>
+                    <div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                        <form>
+                            <!-- Email input -->
+                            <div class="form-outline mb-4">
+                                <label class="form-label" for="form1Example13">Email</label>
+                                <input type="email" id="form1Example13" class="form-control form-control-lg" />
+                            </div>
+
+                            <!-- Password input -->
+                            <div class="form-outline mb-4">
+                                <label class="form-label" for="form1Example23">Password</label>
+                                <input type="password" id="form1Example23" class="form-control form-control-lg" />
+                            </div>
+
+                            <div class="d-flex justify-content-around align-items-center mb-4">
+                                <!-- Checkbox -->
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="" id="form1Example3"
+                                        checked />
+                                    <label class="form-check-label" for="form1Example3"> Ricordami </label>
+                                </div>
+                                <a href="#!">Password dimenticata ?</a>
+                            </div>
+
+                            <!-- Submit button -->
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Accedi</button>
+
+                            <div class="divider d-flex align-items-center my-4">
+                                <p class="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+                            </div>
+
+                            <a class="btn btn-primary btn-lg btn-block btn-login mb-3" style="background-color: #3b5998"
+                                href="#!" role="button">
+                                <i class="fab fa-facebook-f me-2"></i>Continua con Facebook
+                            </a>
+                            <a class="btn btn-primary btn-lg btn-block btn-login" style="background-color: #55acee"
+                                href="#!" role="button">
+                                <i class="fab fa-twitter me-2"></i>Continua con Twitter</a>
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+
+
+
+
+
+
 
 
 
