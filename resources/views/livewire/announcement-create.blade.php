08885ba869f5fac10e20fed1d5df9c2664cc2ede
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-09-22 21:27:37 +0200

    front-end create views

--- resources/views/livewire/announcement-create.blade.php
+++ resources/views/livewire/announcement-create.blade.php
@@ -1,8 +1,8 @@
-<div>
+{{-- <div>
     <div class="container my-5">
         <div class="row">
             <div class="col-12">
-                @if(session()->has('message'))
+                @if (session()->has('message'))
                     <div class="flex flex-row justify-center my2 alert alert-success">
                     {{session('message')}}
                     </div>
@@ -11,28 +11,28 @@
                     @csrf
                     <div class="card col-12">
                         <div class="card-body">
-                            
+
                         <div class="card-title text-center display-5 my-3">Crea un nuovo Annuncio</div>
                             <div class="mb-3">
                                 <label for="title" class="form-label">Titolo</label>
                                 <input value="{{$title}}" wire:model="title" type="text" class="form-control @error('title') is-invalid @enderror" id="title" placeholder="scrivi il titolo dell'annuncio....">
                                 @error('title')
                                     {{$message}}
-                                @enderror    
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="description" class="form-label">Descrizione</label>
                                 <textarea wire:model="description" class="form-control @error('description') is-invalid @enderror" id="description" rows="10" placeholder="scrivi la descrizione dell'annuncio....">{{$description}}</textarea>
                                 @error('description')
                                     {{$message}}
-                                @enderror  
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="price" class="form-label">Prezzo</label>
                                 <input value="{{$price}}" wire:model="price" type="number" class="form-control @error('price') is-invalid @enderror" id="price" placeholder="scrivi il prezzo....">
                                 @error('price')
                                     {{$message}}
-                                @enderror  
+                                @enderror
                             </div>
                             <div class="mb-4">
                                 <label for="category">Categoria</label>
@@ -52,13 +52,13 @@
                                     {{$message}}
                                 @enderror
                             </div>
-                            @if(!empty($images))
+                            @if (!empty($images))
 
                             <div class="row">
                                 <div class="col 12">
                                     <p>photo preview</p>
                                     <div class="row border border-4 border-info rounded shadow py-4">
-                                        @foreach($images as $key => $image)
+                                        @foreach ($images as $key => $image)
                                             <div class="col my-3">
                                                  <div class="mx-auto shadow rounded img-preview" style="background-image:url({{$image->temporaryUrl()}});">
                                                     <button type="button" class="btn btn-danger shadow d-block text-center mt-2 mx-auto" wire:click="removeImage({{$key}})">Cancella</button>
@@ -71,11 +71,55 @@
                             @endif
                             <div class="col-auto">
                                 <button wire:click='store' type="button" class="btn btn-primary mb-3">Aggiungi</button>
-                            </div> 
-                            <p class="{{$color}}">{{$text}}</p>  
+                            </div>
+                            <p class="{{$color}}">{{$text}}</p>
                     </div>
                 </form>
             </div>
         </div>
     </div>
+</div> --}}
+
+<div class="container-fluid">
+    <div class="row ">
+        <!-- The image half -->
+        <div class="col-md-6 d-none d-md-flex bg-image">
+            {{-- <img src="{{ asset('images/create_announcement.png') }}" alt=""> --}}
+        </div>
+
+
+        <!-- The content half -->
+        <div class="col-md-6 bg-white">
+            <div class="login d-flex align-items-center py-5">
+
+                <!-- Demo content-->
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-10 col-xl-7 mx-auto">
+                            <h3 class="display-4">Split page!</h3>
+                            <p class="text-muted mb-4">Create a login split page using Bootstrap 4.</p>
+                            <form>
+                                <div class="form-group mb-3">
+                                    <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary">
+                                </div>
+                                <div class="custom-control custom-checkbox mb-3">
+                                    <input id="customCheck1" type="checkbox" checked class="custom-control-input">
+                                    <label for="customCheck1" class="custom-control-label">Remember password</label>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
+                                <div class="text-center d-flex justify-content-between mt-4"><p>Snippet by <a href="https://bootstrapious.com/snippets" class="font-italic text-muted">
+                                        <u>Boostrapious</u></a></p></div>
+                            </form>
+                        </div>
+                    </div>
+                </div><!-- End -->
+
+            </div>
+        </div><!-- End -->
+
+    </div>
 </div>
+
