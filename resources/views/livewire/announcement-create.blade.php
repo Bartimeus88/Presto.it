e68ffc8641a57422e996d81f09f7d212870f2bbb
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-09-22 23:57:32 +0200

    front-end create announcements page

--- resources/views/livewire/announcement-create.blade.php
+++ resources/views/livewire/announcement-create.blade.php
@@ -96,22 +96,85 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-10 col-xl-7 mx-auto">
-                            <h3 class="display-4">Split page!</h3>
-                            <p class="text-muted mb-4">Create a login split page using Bootstrap 4.</p>
-                            <form>
+                            <h3 class="display-4">Crea</h3>
+                            <p class="text-muted mb-4">Ricordati di compilare tutti i campi</p>
+                            <form wire:submit.prevent="store">
+                                @csrf
                                 <div class="form-group mb-3">
-                                    <input id="inputEmail" type="email" placeholder="Email address" required="" autofocus="" class="form-control rounded-pill border-0 shadow-sm px-4">
+                                    <input value="{{ $title }}" wire:model="title" id="title" type="text" placeholder="Titolo" 
+                                        
+                                        class="form-control rounded-2 border-0 px-4 @error('title') is-invalid @enderror shadow">
+                                    @error('title')
+                                        {{ $message }}
+                                    @enderror
                                 </div>
                                 <div class="form-group mb-3">
-                                    <input id="inputPassword" type="password" placeholder="Password" required="" class="form-control rounded-pill border-0 shadow-sm px-4 text-primary">
+                                    <textarea wire:model="description" class="form-control @error('description') is-invalid @enderror" id="description"
+                                        type="password" placeholder="scrivi la descrizione dell'annuncio...." required=""
+                                        class="form-control border-0 form-control rounded-2 shadow px-4 text-primary">{{ $description }}</textarea>
+                                    @error('description')
+                                        {{ $message }}
+                                    @enderror
                                 </div>
-                                <div class="custom-control custom-checkbox mb-3">
-                                    <input id="customCheck1" type="checkbox" checked class="custom-control-input">
-                                    <label for="customCheck1" class="custom-control-label">Remember password</label>
+                                <div class="form-group mb-3">
+                                    <input value="{{ $price }}" wire:model="price" id="price" type="number"
+                                        placeholder="prezzo" required="" autofocus=""
+                                        class="form-control rounded-2 border-0 px-4 @error('price') is-invalid @enderror shadow">
+                                    @error('price')
+                                        {{ $message }}
+                                    @enderror
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-block text-uppercase mb-2 rounded-pill shadow-sm">Sign in</button>
-                                <div class="text-center d-flex justify-content-between mt-4"><p>Snippet by <a href="https://bootstrapious.com/snippets" class="font-italic text-muted">
-                                        <u>Boostrapious</u></a></p></div>
+                                <div class="form-group mb-3">
+                                    <select wire:model.defer="category" id="category"
+                                        class="form-control border-0 form-select @error('price') is-invalid @enderror shadow"
+                                        aria-label="Default select example">
+                                        <option selected>Scegli la categoria</option>
+                                        @foreach ($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    @error('category')
+                                        {{ $message }}
+                                    @enderror
+                                </div>
+                                <div class="upload__box mt-2">
+                                    <div class="upload__btn-box">
+                                        <label class="upload__btn">
+                                            <p class="mb-2">Inserisci una immagine</p>
+                                            <input wire:model="temporary_images" type="file" name="images" multiple
+                                                class="upload_inputfile form-control shadow @error('temporary_images.*') is-invalid @enderror"
+                                                placeholder="Img"">
+                                            @error('temporary_images.*')
+                                                {{ $message }}
+                                            @enderror
+                                        </label>
+                                    </div>
+                                    <div class="upload__img-wrap">
+                                        @if (!empty($images))
+
+                                            <div class="row">
+                                                <div class="col 12">
+                                                    <p class="mt-3 pb-3">photo preview</p>
+                                                    <div class="row border border-4 border-dark rounded shadow py-4">
+                                                        @foreach ($images as $key => $image)
+                                                            <div class="col my-3">
+                                                                <div class="mx-auto shadow rounded img-preview"
+                                                                    style="background-image:url({{ $image->temporaryUrl() }});">
+                                                                    <button type="button"
+                                                                        class="btn btn-danger shadow d-block text-center mt-2 mx-auto"
+                                                                        wire:click="removeImage({{ $key }})">Cancella</button>
+                                                                </div>
+                                                        @endforeach
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        @endif
+                                    </div>
+                                </div>
+                                <button type="button" wire:click='store' type="button"
+                                    class="btn btn-dark btn-block text-withe mb-2 rounded-2 shadow mt-3">Aggiungi
+                                </button>
+                                <p class="{{ $color }}">{{ $text }}</p>
                             </form>
                         </div>
                     </div>
@@ -122,4 +185,3 @@
 
     </div>
 </div>
-
