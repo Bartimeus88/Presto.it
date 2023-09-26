4cc93e409ee4b4fd38b74dce6f4589ad724bf521
Bartimeus88 <luca.lovisa8@gmail.com>
2023-09-26 21:40:22 +0200

    pagina 404
    pagina 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,17 @@
+<x-layout>
+<br><br><br><br><br><br><br><br><br>
+
+<div class="container my-5 py-5">
+    <div class="row">
+        <div class="col 12 text-center">
+            <h2 class="display-2">Pagina non trovata</h2>
+        </div>
+        <div class="col-12 text-center">
+            <a class="btn btn-dark" href="/">Torna alla homepage</a>
+        </div>
+    </div>
+</div>
+
+
+
+</x-layout>
