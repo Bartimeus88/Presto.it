9211beda76be71e276cf911c13850541f1ae99d4
Gellart Trefilaza <130781666+Gellart@users.noreply.github.com>
2023-09-28 04:19:54 +0200

    front-end
    - pagina profilo utente
    - pagina errore

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-<br><br><br><br><br><br><br><br><br>
+    {{-- <br><br><br><br><br><br><br><br><br>
 
 <div class="container my-5 py-5">
     <div class="row">
@@ -10,7 +10,18 @@
             <a class="btn btn-dark" href="/">Torna alla homepage</a>
         </div>
     </div>
-</div>
+</div> --}}
+
+    <div id="notfound">
+        <div class="notfound">
+            <div class="notfound-404">
+                <h1>4<span></span>4</h1>
+            </div>
+            <h2>Pagina non trovata</h2>
+            <p>Niente panico! Clicca qu' sotto per tornare alla home</p>
+            <a class="bt-dark" href="/">Torna alla homepage</a>
+        </div>
+    </div>
 
 
 
